7befbd2e7eb5881c1b2c2be423dc8aeb7d23cc78
cmartinez1309 <cmartinez1309@gmail.com>
2022-03-22 18:28:45 -0600

    FaceUTEQ

---
+++ src/main/java/com/example/faceUTEQ/FaceUteqApplication.java
@@ -0,0 +1,13 @@
+package com.example.faceUTEQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceUteqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceUteqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/faceUTEQ/FaceUteqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faceUTEQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceUteqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
