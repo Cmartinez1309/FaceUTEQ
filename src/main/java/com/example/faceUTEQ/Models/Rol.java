70a01554bff071ce8831947e583b0be194345551
GusatvoAngel <84344928+GusatvoAngel@users.noreply.github.com>
2022-03-23 00:12:07 -0600

    models

---
+++ src/main/java/com/example/faceUTEQ/Models/Rol.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.faceUTEQ.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author usuario
+ */
+@Entity
+@Table(name = "rol")
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_rol;
+
+    @NotEmpty
+    @NotNull(message = "Debes especificar el nombre")
+    @Size(min = 1, max = 50, message = "El nombre debe medir entre 1 y 50")
+    private String nombre;
+
+}
---
+++ src/main/java/com/example/faceUTEQ/Models/Usuario.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.faceUTEQ.Models;
+
+import java.util.Collection;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author usuario
+ */
+@Entity
+@Table(name = "usuario", uniqueConstraints = @UniqueConstraint(columnNames = "Correo"))
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id_usu;
+
+    @NotEmpty
+    @NotNull(message = "Debes especificar el cooreo")
+    @Size(min = 1, message = "El correo no debe ser nulo")
+    private String Correo;
+
+    @NotEmpty
+    @NotNull(message = "Debes especificar la contrasena")
+    @Size(min = 1, max = 50, message = "La contrasena debe medir entre 1 y 50")
+    private String Contasena;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "usuario_roles",
+            joinColumns = @JoinColumn(name = "id_usu", referencedColumnName = "id_usu"),
+            inverseJoinColumns = @JoinColumn(name = "id_rol", referencedColumnName = "id_rol")
+    )
+    private Collection<Rol> roles;
+}
